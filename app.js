1a5401f28c45fefb226bf0b5918d9faf1fe8b2f2
yogiduzit <yverma2@my.bcit.ca>
2020-01-12 07:35:07 -0800

    Basic Workflow working

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ const app = express();
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.join("public")));
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   let doc = fs.readFileSync("./public/html/index.html", "utf-8");
   res.send(doc);
 });
 
 app.get('/map', (req, res) => {
-  let doc = fs.readFileSync("./public/html/index.html", "utf-8");
+  let doc = fs.readFileSync("./public/html/maps.html", "utf-8");
+  res.send(doc);
+});
+
+app.get('/', (req, res) => {
+  let doc = fs.readFileSync("./public/html/brian_index.html", "utf-8");
   res.send(doc);
 });
 
