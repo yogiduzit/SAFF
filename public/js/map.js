06ccd057e6be036f6b550b8f1c1f60b5de3f2d74
yogiduzit <yverma2@my.bcit.ca>
2020-01-11 20:21:53 -0800

    Map showing up

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,11 @@
+
+const APIKEY = "adf68c474262738c411594f324f0d792b92077232dfa1ebda30cb108"
+const RACKS_URL = "https://opendata.vancouver.ca/api/records/1.0/search?dataset=bike-racks"
+
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.248499, lng: -123.001375},
-    zoom: 11,
+    zoom: 1,
     mapTypeId: 'roadmap'
   });
 
@@ -55,48 +59,51 @@ function initAutocomplete() {
   }
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', async function() {
     var places = searchBox.getPlaces();
 
     if (places.length == 0) {
       return;
     }
-    //
-    // // Clear out the old markers.
-    // markers.forEach(function(marker) {
-    //   marker.setMap(null);
-    // });
-    // markers = [];
+    const searchedPlace = places[0].geometry.location;
+    places = await getBikeRacks();
+
+    console.log(places);
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
+    places.records.forEach(record => {
+      const geocoder = new google.maps.Geocoder();
+      if (!record.fields) {
         console.log("Returned place contains no geometry");
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+      const address = `${record.fields.street_number}.${record.fields.street_name},Vancouver`;
+      const position = codeAddress(geocoder, address);
 
-      // // Create a marker for each place.
-      // markers.push(new google.maps.Marker({
-      //   map: map,
-      //   icon: icon,
-      //   title: place.name,
-      //   position: place.geometry.location
-      // }));
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        position: position
+      }));
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
     });
-    map.fitBounds(bounds);
   });
+
+const codeAddress = (geocoder, address) => {
+  geocoder.geocode({ 'address': address }, (results, status) => {
+      if (status == 'OK') {
+        return results[0].geometry.location
+      } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+      }
+  });
+}
+
+const getBikeRacks = async () => fetch(`${RACKS_URL}&rows=10000&apikey=${APIKEY}`).then(res => res.json());
