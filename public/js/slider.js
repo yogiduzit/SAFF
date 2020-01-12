124a97f801c850cabed306273956946b2f1cf469
Cyra <cyraa@live.ca>
2020-01-11 18:45:26 -0800

    slider has slide up function

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,3 +1,9 @@
 // Javascript File for slider with location info
 
 const slide = document.getElementById('slide');
+const slider = document.getElementById('slider');
+
+slide.addEventListener('click', e => {
+	e.preventDefault();
+	slider.classList.add('expand');
+});
